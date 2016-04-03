0c41cfad64946d563e7891764a08e47ad7e2bf37
Kangwon Lee <kangwon_fwd@naver.com>
2016-04-04 00:07:02 +0900

    added TODOs

--- occupancy.py
+++ occupancy.py
@@ -1,3 +1,9 @@
 """
 To calculate how much bytes each folders use
 """
+# TODO : os.walk loop
+# TODO : folder size
+# TODO : Recursive summation?
+# TODO : Data presentation in GUI?
+# TODO : Data presentation : in Bytes & in percentage
+
