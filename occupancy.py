ea536d3867464a7a2d1cadd4de556bd60c929a04
Kangwon Lee <kangwon_fwd@naver.com>
2016-04-04 00:40:47 +0900

    added TODOs using info from http://stackoverflow.com/questions/1392413/calculating-a-directory-size-using-python

--- occupancy.py
+++ occupancy.py
@@ -2,7 +2,12 @@
 To calculate how much bytes each folders use
 """
 # TODO : os.walk loop
-# TODO : folder size
+# TODO : folder size :
+#           monkut, Calculating a directory size using Python?, Sep 8 '09,
+#           http://stackoverflow.com/questions/1392413/calculating-a-directory-size-using-python
+# TODO : Consider applying scandir for Python 3.x
+#           monkut, Calculating a directory size using Python?, Jan 29 '09,
+#           http://stackoverflow.com/questions/1392413/calculating-a-directory-size-using-python
 # TODO : Recursive summation?
 # TODO : Data presentation in GUI?
 # TODO : Data presentation : in Bytes & in percentage
