ff47e95d6130f335a1099f9ee22da0db47da2083
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2016-04-04 15:07:06 +0900

    added reference to os.scandir() https://docs.python.org/3.5/library/os.html#os.scandir

--- occupancy.py
+++ occupancy.py
@@ -4,10 +4,12 @@ To calculate how much bytes each folders use
 # TODO : os.walk loop
 # TODO : folder size :
 #           monkut, Calculating a directory size using Python?, Sep 8 '09,
-#           http://stackoverflow.com/questions/1392413/calculating-a-directory-size-using-python
+#               http://stackoverflow.com/questions/1392413/calculating-a-directory-size-using-python
 # TODO : Consider applying scandir for Python 3.x
 #           monkut, Calculating a directory size using Python?, Jan 29 '09,
-#           http://stackoverflow.com/questions/1392413/calculating-a-directory-size-using-python
+#               http://stackoverflow.com/questions/1392413/calculating-a-directory-size-using-python
+#           16.1. os — Miscellaneous operating system interfaces, Feb 27 '16,
+#               https://docs.python.org/3.5/library/os.html#os.scandir
 # TODO : Recursive summation?
 # TODO : Data presentation in GUI?
 # TODO : Data presentation : in Bytes & in percentage
